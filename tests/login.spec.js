78a72133f7ba1c5b72e1da4d625a89d2024ece17
first-repo <kpandapanigrahi@gmail.com>
2026-07-09 14:57:14 +0530

    initial playwright project

---
+++ tests/login.spec.js
@@ -0,0 +1,7 @@
+const { test, expect } = require('@playwright/test')
+test('login test', async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/login')
+    const username = await page.locator('#username').fill('tomsmith')
+    const password = await page.locator('#password').fill('SuperSecretPassword!')
+    await page.getByText('Login').nth(1).click()
+})

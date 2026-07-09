706ca8a2ba7cbb88386eee08c33e573092312fa5
first-repo <kpandapanigrahi@gmail.com>
2026-07-09 17:55:24 +0530

    fixed tracking and sync

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,5 +1,5 @@
 const { test, expect } = require('@playwright/test')
-test('login test updated', async ({ page }) => {
+test('login test new', async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/login')
     const username = await page.locator('#username').fill('tomsmith')
     const password = await page.locator('#password').fill('SuperSecretPassword!')
